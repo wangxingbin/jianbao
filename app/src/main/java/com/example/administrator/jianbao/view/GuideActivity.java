9185bf0a9a8102bdcbfa72d2ce5508b2b080ac61
SuperDHQ <dhq0317@126.com>
2016-11-25 16:15:32 +0800

    hahah

---
+++ app/src/main/java/com/example/administrator/jianbao/view/GuideActivity.java
@@ -0,0 +1,20 @@
+package com.example.administrator.jianbao.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.administrator.jianbao.R;
+
+/**
+ * Created by Administrator on 2016/11/24.
+ */
+
+public class GuideActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+        
+
+    }
+}
