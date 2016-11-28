1a5bedb2e7da2a92605dbba27a03d2f0d0bdf75a
wangxingbin <100815411@qq.com>
2016-11-28 14:15:54 +0800

    贱宝项目包

--- app/src/main/java/mainframe/MainFrameAcitvity.java
+++ app/src/main/java/mainframe/MainFrameAcitvity.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 
 import com.wxb.jianbao.R;
 
-import fragment.AddFragment;
 import fragment.MineFragment;
 import fragment.ShowFragment;
 
@@ -24,20 +23,13 @@ import fragment.ShowFragment;
 public class MainFrameAcitvity extends FragmentActivity {
 
     private MineFragment mine;//个人主页
-    private AddFragment add;//发布
-    /**
-     * 具体内容
-     */
+
     private android.support.v4.app.Fragment contentFragment;
-    /**
-     * 管理fragment
-     */
     private FragmentManager fragmentManager;
+
     private TextView shouye_tv;
     private TextView mine_tv;
-    private TextView add_tv;
     private ImageView shouye;
-    private ImageView tianjia;
     private ImageView geren;
 
     @Override
@@ -49,15 +41,12 @@ public class MainFrameAcitvity extends FragmentActivity {
 
     private void initView() {
         shouye = (ImageView) findViewById(R.id.iv_shouye);
-        tianjia = (ImageView) findViewById(R.id.iv_tianjia);
         geren = (ImageView) findViewById(R.id.iv_geren);
 
         shouye_tv = (TextView) findViewById(R.id.tv_zhuye);
-        add_tv = (TextView) findViewById(R.id.tv_add);
         mine_tv = (TextView) findViewById(R.id.tv_mine);
 
         shouye.setOnClickListener(itemClick);
-        tianjia.setOnClickListener(itemClick);
         geren.setOnClickListener(itemClick);
 
         fragmentManager = getSupportFragmentManager();
@@ -74,11 +63,7 @@ public class MainFrameAcitvity extends FragmentActivity {
                     contentFragment = new ShowFragment();
                     transaction.replace(R.id.main_frame_layout, contentFragment);
                     break;
-                case R.id.iv_tianjia:
-                    initAdd();
-                    contentFragment = new AddFragment();
-                    transaction.replace(R.id.main_frame_layout, contentFragment);
-                    break;
+
                 case R.id.iv_geren:
                     initMine();
                     contentFragment = new MineFragment();
@@ -94,26 +79,12 @@ public class MainFrameAcitvity extends FragmentActivity {
     private void initShouye() {
         shouye.setBackgroundResource(R.mipmap.home2);
         shouye_tv.setTextColor(Color.parseColor("#000000"));
-        tianjia.setBackgroundResource(R.mipmap.add);
-        add_tv.setTextColor(Color.parseColor("#a9b7b7"));
         geren.setBackgroundResource(R.mipmap.mine);
         mine_tv.setTextColor(Color.parseColor("#a9b7b7"));
     }
-
-    private void initAdd() {
-        shouye.setBackgroundResource(R.mipmap.home);
-        shouye_tv.setTextColor(Color.parseColor("#a9b7b7"));
-        tianjia.setBackgroundResource(R.mipmap.add2);
-        add_tv.setTextColor(Color.parseColor("#000000"));
-        geren.setBackgroundResource(R.mipmap.mine);
-        mine_tv.setTextColor(Color.parseColor("#a9b7b7"));
-    }
-
     private void initMine() {
         shouye.setBackgroundResource(R.mipmap.home);
         shouye_tv.setTextColor(Color.parseColor("#a9b7b7"));
-        tianjia.setBackgroundResource(R.mipmap.add);
-        add_tv.setTextColor(Color.parseColor("#a9b7b7"));
         geren.setBackgroundResource(R.mipmap.mine2);
         mine_tv.setTextColor(Color.parseColor("#000000"));
     }
