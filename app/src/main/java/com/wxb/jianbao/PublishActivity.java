144e00124995d34a3a3dcd770635a8c5cb4b65a9
ZhaoUnruly <android_zzq@163.com>
2016-12-02 15:50:12 +0800

    赵不羁：zuixin

--- app/src/main/java/com/wxb/jianbao/PublishActivity.java
+++ app/src/main/java/com/wxb/jianbao/PublishActivity.java
@@ -6,7 +6,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -26,11 +29,13 @@ public class PublishActivity extends Activity {
     private ImageView publish_imgs4;
     private ImageView publish_imgs5;
     private ImageView[] imageViews = new ImageView[5];
-    private List<Fragment> list=new ArrayList<Fragment>();
-
+    private List<Fragment> list = new ArrayList<Fragment>();
     private int mCurrentPageIndex;
     private int mScreen1_3;
-   //  private ScrollableLayout mScrollLayout;
+    private EditText et_jianbao_biaoti;
+    private EditText et_jianbao_miaoshu;
+
+    //  private ScrollableLayout mScrollLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,16 +75,53 @@ public class PublishActivity extends Activity {
         imageViews[2] = publish_imgs3;
         imageViews[3] = publish_imgs4;
         imageViews[4] = publish_imgs5;
-        EditText et_jianbao_biaoti = (EditText) findViewById(R.id.et_jiianbao_biaoti);//标题
-        initEditTextLenght();
-        et_jianbao_biaoti.setFilters(new InputFilter[]{new InputFilter.LengthFilter(10)}); //最多输入10的字符
-        EditText et_jianbao_miaoshu = (EditText) findViewById(R.id.et_jianbao_miaoshu); //描述
-        et_jianbao_biaoti.setFilters(new InputFilter[]{new InputFilter.LengthFilter(30)}); //最多输入30的字符
+
+        et_jianbao_biaoti = (EditText) findViewById(R.id.et_jiianbao_biaoti); //标题
+        et_jianbao_miaoshu = (EditText) findViewById(R.id.et_jianbao_miaoshu); //描述
         EditText et_jianbao_number = (EditText) findViewById(R.id.et_jianbao_number); //手机号
         EditText et_jianbao_price = (EditText) findViewById(R.id.et_jianbao_price);// 价格
+        et_jianbao_biaoti.addTextChangedListener(textWatcher);
+        et_jianbao_miaoshu.addTextChangedListener(textWatcher);
+        initUploading();
+    }
+    // 上传数据
+    private void initUploading() {
+
     }
 
-    private void initEditTextLenght() {
-    }
+    // 判断Edittext最多输入多少字符
+    TextWatcher textWatcher = new TextWatcher() {
 
+        private Toast toast;
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after) {
+            //TODO
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before,
+                                  int count) {
+            //TODO
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+            Log.d("TAG", "afterTextChanged    " + "str=" + s.toString());
+            int len = et_jianbao_biaoti.getText().toString().length();
+            int len2 = et_jianbao_miaoshu.getText().toString().length();
+            if (len >= 9) {
+                Toast.makeText(PublishActivity.this, "亲,最多输入10个字呦", Toast.LENGTH_SHORT).show();
+                et_jianbao_biaoti.setFilters(new InputFilter[]{new InputFilter.LengthFilter(10)}); //最多输入10的字符
+            }
+            if (len >= 29) {
+                Toast.makeText(PublishActivity.this, "亲,最多输入10个字呦", Toast.LENGTH_SHORT).show();
+                et_jianbao_miaoshu.setFilters(new InputFilter[]{new InputFilter.LengthFilter(30)}); //最多输入30的字符
+            }
+        }
+    };
 }
+
+
